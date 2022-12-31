bc757c08ef0ad35629149e23d46a76947b594529
Sivabokam7482 <sivabokam7482@gmail.com>
2022-12-31 12:26:07 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Nivi(request):
+    return render(request,'Nivi.html')
+
+def siva(request):
+    return render(request,'siva.html')
+    
+    
